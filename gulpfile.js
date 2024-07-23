496c32b79fc7d0bb1cbc79a4de24bbe77c445e12
eche-unicorn <d.anasteysha@gmail.com>
2024-07-24 00:47:52 +0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,27 @@
 const { parallel, series, src, dest, watch } = require("gulp");
 
 const sass = require("gulp-sass")(require("sass"));
-// const autoprefixer = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
 const minifier = require("gulp-clean-css");
 const rename = require("gulp-rename");
 
 const browserSync = require("browser-sync").create();
 
 function compileScss() {
-  return (
-    src("./src/scss/*.scss")
-      .pipe(sass())
-      // .pipe(autoprefixer())
-      .pipe(minifier())
-      .pipe(
-        rename(function (path) {
-          return {
-            dirname: path.dirname + "",
-            basename: path.basename + ".min",
-            extname: ".css",
-          };
-        })
-      )
-      .pipe(dest("./dist/css"))
-    // .pipe(browserSync.reload({ stream: true }))
-  );
+  return src("./src/scss/*.scss")
+    .pipe(sass())
+    .pipe(minifier())
+    .pipe(
+      rename(function (path) {
+        return {
+          dirname: path.dirname + "",
+          basename: path.basename + ".min",
+          extname: ".css",
+        };
+      })
+    )
+    .pipe(dest("./dist/css"));
+  // .pipe(browserSync.reload({ stream: true }))
 }
 
 function html() {
@@ -35,14 +32,19 @@ function html() {
   );
 }
 
+function js() {
+  return src("src/js/**/*.js").pipe(uglify()).pipe(dest("./dist/js"));
+}
+
 exports.html = html;
 
 function taskWatch() {
   // browserSync.init({ server: { baseDir: "dist" }, injectChanges: true });
   watch("src/*.html", html);
   watch("./src/scss/*.scss", compileScss);
+  watch("./src/js/**/*.js", js);
 }
 
 exports.taskWatch = taskWatch;
 
-exports.default = series(parallel(html), compileScss, taskWatch);
+exports.default = series(parallel(html), js, compileScss, taskWatch);
