91bc0871e41d577c1b10679fa14d3987ab9ab559
eche-unicorn <d.anasteysha@gmail.com>
2024-07-24 19:29:28 +0300

    Updates

--- js/offerCard.js
+++ js/offerCard.js
@@ -1,42 +1,23 @@
-export default function offerCard({
-  amount,
-  license_name,
-  name_prod,
-  price_key,
-  is_best,
-  link,
-}) {
-  return `
-        <li class="offer__card">
+export default function offerCard({amount:i,license_name:s,name_prod:e,price_key:a,is_best:c,link:d}){return`<li class="offer__card">
           <div class="price__container">
-          ${is_best ? `<div class="price__best">Best Value</div>` : ""}
+          ${c?'<div class="price__best">Best Value</div>':""}
           
-          ${
-            price_key?.includes("%")
-              ? `<div class="price__discount">
+          ${a?.includes("%")?`<div class="price__discount">
           <div class="price__ribbon">
-          <div class="ribbon"> <div> <p class="price_discount-amount">${price_key}</p>
+          <div class="ribbon"> <div> <p class="price_discount-amount">${a}</p>
               
           <p>OFF</p></div> </div>
           </div>
-          </div>`
-              : ""
-          }
-         <div class="price__text-wrapper"> <p class="price__amount">$${amount}</p>
-          ${
-            price_key?.includes("%")
-              ? `<p class="price__original">
-          $${+amount * (100 / price_key.replace(/%/g, ""))} 
-          </p>`
-              : ""
-          }</div>
+          </div>`:""}
+         <div class="price__text-wrapper"> <p class="price__amount">$${i}</p>
+          ${a?.includes("%")?`<p class="price__original">
+          $${+i*(100/a.replace(/%/g,""))} 
+          </p>`:""}</div>
           
           </div>
-          <div class="description__container"><p>${name_prod}</p>
-          <p class="description__license">${license_name}</p>
-         </div> <a href="${link}" target="_blank" class="offer__download">
+          <div class="description__container"><p>${e}</p>
+          <p class="description__license">${s}</p>
+         </div> <a href="${d}" target="_blank" class="offer__download">
          <span>Download</span> <img src="../src/img/download.png" alt="Download" height="30" width="30"/>
          </a>
-        </li>
-      `;
-}
+        </li>`}
--- js/refs.js
+++ js/refs.js
@@ -1,5 +1,1 @@
-export default {
-  arrow: document.querySelector(".arrow"),
-  loader: document.querySelector(".loader"),
-  list: document.querySelector(".offers"),
-};
+export default{arrow:document.querySelector(".arrow"),arrowWrapper:document.querySelector(".arrow__wrapper"),loader:document.querySelector(".loader"),list:document.querySelector(".offers")};
