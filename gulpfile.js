88a95dd4ee7696d807641d18ce921e406c821add
eche-unicorn <d.anasteysha@gmail.com>
2024-07-24 22:13:02 +0300

    js min

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 const { parallel, series, src, dest, watch } = require("gulp");
 
 const sass = require("gulp-sass")(require("sass"));
-const uglify = require("gulp-uglify");
+const terser = require("gulp-terser");
 const minifier = require("gulp-clean-css");
 const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
 
-const browserSync = require("browser-sync").create();
+function html() {
+  return src("src/*.html", { allowEmpty: true }).pipe(dest("./dist"));
+}
 
-function compileScss() {
+function scss() {
   return src("./src/scss/*.scss")
     .pipe(sass())
     .pipe(minifier())
@@ -21,30 +24,30 @@ function compileScss() {
       })
     )
     .pipe(dest("./dist/css"));
-  // .pipe(browserSync.reload({ stream: true }))
-}
-
-function html() {
-  return (
-    src("src/*.html", { allowEmpty: true })
-      // .pipe(browserSync.reload({ stream: true }))
-      .pipe(dest("./dist"))
-  );
 }
 
 function js() {
-  return src("src/js/**/*.js").pipe(uglify()).pipe(dest("./dist/js"));
+  return src("src/js/*.js").pipe(terser()).pipe(dest("./dist/js"));
 }
 
-exports.html = html;
+function imgs() {
+  return src("./src/img/*.{png,jp(e)g}")
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 2 }),
+        imagemin.mozjpeg({ quality: 80, progressive: true }),
+      ])
+    )
+    .pipe(dest("./dist/img"));
+}
 
 function taskWatch() {
-  // browserSync.init({ server: { baseDir: "dist" }, injectChanges: true });
   watch("src/*.html", html);
-  watch("./src/scss/*.scss", compileScss);
-  watch("./src/js/**/*.js", js);
+  watch("./src/scss/*.scss", scss);
+  watch("./src/js/*.js", js);
+  watch("./src/img/*.{png,jp(e)g}", js);
 }
 
 exports.taskWatch = taskWatch;
 
-exports.default = series(parallel(html), js, compileScss, taskWatch);
+exports.default = series(parallel(html), js, scss, imgs, taskWatch);
